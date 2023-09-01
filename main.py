de2680c35c762117af9fc864de32d754e44c9a69
divejane <sonny.nichols5689@gmail.com>
2023-08-31 21:42:18 -0500

    attempt to complete all client <-> server code

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
+# TODO: room_gen structure needs the host to remain in a listening state for connections instead of just
+# creating the room and leaving it   
+# TODO: fix "ran out of input" on client and similiar error on server, likely issue with incorrect stack on data transfer TRY: db commenting the if deleterequest
+# TODO: remove all db lines, including the fake hostlist in the server 
+
 import socket
 import pickle 
 import os
 
-HOST = "18.224.63.138"
-PORT = 5222
+HOST = "3.21.248.228"
+PORT = 9236
 username = "anon"
 
 cls = lambda: os.system('cls' if os.name=='nt' else 'clear')
 
-def point_check(max):
+def point_check(max): # return user input if within a range
     while True:
         try: usinp = int(input("$: "))
-        except KeyboardInterrupt: quit()
+        except KeyboardInterrupt: quit() # ^C break
         except: continue
-        if 0 < usinp < max: return usinp
+        if 0 <= usinp <= max: return usinp
 
-def room_join():
+def room_join(): # List room names 
+    # Request list of rooms from server
     cls()
-    roomjoin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    roomjoin_socket.connect((HOST, PORT))
-    roomjoin_socket.sendall(b'roomrequest')
-    open_hosts = roomjoin_socket.recv(1024)
+    roomrequest_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    roomrequest_socket.connect((HOST, PORT))
+    roomrequest_socket.sendall(b'roomrequest')
+    open_hosts = pickle.loads(roomrequest_socket.recv(1024))
+    roomrequest_socket.close()
+
+    # Print room names w/ formatting 
+    print('|     room name     |\n')
+    for x, roomname in enumerate(open_hosts[1:]):
+        print(f'{x+1}) {roomname[2]}')
+    print('____________________')
+    print('\nenter room to join (0 to exit) -')
+    usinp = point_check(len(open_hosts)-1) # db change -1 if last room cant be selected
+
+    # Exit if usinp == 0
+    if usinp == 0: 
+        main()
+
+    # Send request to server to delete room information 
+    else:
+        deleterequest = [b'deleterequest', usinp]
+        roomjoin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        roomjoin_socket.connect((HOST, PORT))
+        roomjoin_socket.sendall(pickle.dumps(deleterequest)) # Send room list index to server with deleterequest
+        roomjoin_socket.close()
     
-    # print open hosts
-        
 def room_gen(): 
     cls()
     print("           _                \n ___ ___  (_)__ ___ _  ___ _\n/ -_) _ \/ / _ `/  ' \/ _ `/\n\__/_//_/_/\_, /_/_/_/\_,_/ \n          /___/             \n\n")
@@ -38,8 +63,8 @@ def room_gen():
             hostgen_socket.close()
             break
         print("room name and password must not be longer than 16 characters")
-    print("server configured, type 1 to exit")
-    if point_check(2) == 1: main() 
+    print("room configured, type 1 to exit")
+    if point_check(1) == 1: main() 
         
 def settings():
     cls()
@@ -47,7 +72,7 @@ def settings():
 
     print("           _                \n ___ ___  (_)__ ___ _  ___ _\n/ -_) _ \/ / _ `/  ' \/ _ `/\n\__/_//_/_/\_, /_/_/_/\_,_/ \n          /___/             \n\n")
     print(f'1) change username ("{username}")\n2) back')
-    usinp = point_check(3)
+    usinp = point_check(2)
     if usinp == 1: 
         username = input("enter username: ")
         settings()
@@ -58,9 +83,9 @@ def main():
     cls()
     
     print("           _                \n ___ ___  (_)__ ___ _  ___ _\n/ -_) _ \/ / _ `/  ' \/ _ `/\n\__/_//_/_/\_, /_/_/_/\_,_/ \n          /___/             \n\n")
-    print('1) join a server\n2) create a server\n3) settings\n')
+    print('1) join a room\n2) create a room\n3) settings\n')
     
-    point = point_check(4)
+    point = point_check(3)
 
     if point == 1: room_join()
     if point == 2: room_gen()
